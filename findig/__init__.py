f861591ce8287c43d887b7893163648f1ceb58e8
Te-jé Rodgers <tjd.rodgers@gmail.com>
2015-04-15 15:45:04 -0400

    Updated findig.App

--- findig/__init__.py
+++ findig/__init__.py
@@ -1,14 +1,13 @@
 from contextlib import contextmanager, ExitStack
 from functools import wraps
-import sys
-from os.path import dirname, join
+from os.path import join, dirname
 
 from werkzeug.local import LocalManager
-from werkzeug.routing import Map
+from werkzeug.routing import Map, RuleFactory
+from werkzeug.utils import cached_property
 
 from findig.context import *
-from findig.data import JSONErrorHandler, JSONFormatter, JSONParser
-from findig.manager import Manager
+from findig.dispatcher import Dispatcher
 from findig.wrappers import Request
 
 
@@ -16,97 +15,97 @@ with open(join(dirname(__file__), "VERSION")) as fh:
     __version__ = fh.read().strip()
 
 
-class App(Manager):
+class App(Dispatcher):
     request_class = Request
     local_manager = LocalManager()
 
 
-    def __init__(self, **args):
-        super(App, self).__init__(**args)
+    def __init__(self):
+        super(App, self).__init__()
 
         self.local_manager.locals.append(ctx)
-
-        self.managers = [self]
         self.context_hooks = []
 
     def context(self, func):
+        """
+        Register a request context manager for the application.
+
+        A request context manager is a function that yields once, that is
+        used to wrap request contexts. It is called at the beginning of a 
+        request context, during which it yields control to Findig, and 
+        regains control sometime after findig processes the request. If 
+        the function yields are value, it is made available as an
+        attribute on ``findig.context.ctx`` with the same name as the
+        function.
+
+        Example::
+
+            >>> from findig.context import ctx
+            >>> from findig import App
+            >>> 
+            >>> app = App()
+            >>> items = []
+            >>> @app.context
+            ... def meaning():
+            ...     items.extend(["Life", "Universe", "Everything"])
+            ...     yield 42
+            ...     items.clear()
+            ...
+            >>> with app.build_context(): # don't use this unless you're testing, but this is how Findig sets up your request context
+            ...     print("The meaning of", end=" ")
+            ...     print(*items, sep=", ", end=": ")
+            ...     print(ctx.meaning)
+            ...
+            The meaning of Life, Universe, Everything: 42
+            >>> items
+            []
+
+        """
         self.context_hooks.append(contextmanager(func))
         return func
 
     def cleanup_hook(self, func):
         @wraps(func)
-        def foo():
+        def wrapper():
             yield
             func()
-        self.context(foo)
+        self.context(wrapper)
+        return func
+
+    def build_context(self):
+        context = ExitStack()
+        context.callback(self.local_manager.cleanup)
+        # Add all the application's context managers to
+        # the exit stack. If any of them return a value,
+        # we'll add the value to the application context
+        # with the function name.
+        for hook in self.context_hooks:
+            retval = context.enter_context(hook())
+            if retval is not None:
+                setattr(ctx, hook.__name__, retval)
+        return context
 
     def __call__(self, environ, start_response):
         # Set up the application context and run the
         # app inside it.
-        with ExitStack() as context:
-            context.callback(self.local_manager.cleanup)
-            
-            # Add all the application's context managers to
-            # the exit stack. If any of them return a value,
-            # we'll add the value to the application context
-            # with the function name.
-            for hook in self.context_hooks:
-                retval = context.enter_context(hook())
-                if retval is not None:
-                    setattr(ctx, hook.__name__, retval)
-
+        with self.build_context():
             return self.wsgi_app(environ, start_response)
 
     def wsgi_app(self, environ, start_response):
-        request = self.request_class(environ)
-        response = self.dispatch(request)
+        """
+        Run the WSGI application
+
+        :param environ: A WSGI environment
+        :param start_response: A WSGI file handle for the HTTP response.
+        :return: A response stream
+        """
+        ctx.app = self
+        ctx.request = request = self.request_class(environ)
+        ctx.url_adapter = adapter = self.url_map.bind_to_environ(environ)
+        rule, url_values = adapter.match(return_rule=True)
+        response = self.dispatch(request, rule, url_values)
         return response(environ, start_response)
 
-    def dispatch(self, request):
-        ctx.app = self
-        ctx.request = request
-        ctx.url_adapter = adapter = self.url_map.bind_to_environ(
-            request.environ)
-
-        try:
-            rule, url_values = adapter.match(return_rule=True)
-
-            # Bind the url values to the resource object that was
-            # stored as the rule endpoint, creating a bound resource.
-            resource = rule.endpoint.bind(**url_values)
-
-            # Ask the resource manager to handle the request
-            response = resource.manager.handle(request, resource)
-
-        except Exception as e:
-            tp, m, tb = sys.exc_info()
-            return self.exceptions.handle(e, tp, m, tb, top_level=True)
-        else:
-            return response
-
-    def add_manager(self, manager):
-        if manager not in self.managers:
-            self.managers.append(manager)
-
-    def rebuild_url_map(self):
-        self._url_map = map = Map()
-
-        for manager in self.managers:
-            map.add(manager)
-        
-        return map
-
-    @property
+    @cached_property
     def url_map(self):
-        if not hasattr(self, '_url_map'):
-            return self.rebuild_url_map()
-        else:
-            return self._url_map
-
-
-class JSONApp(App):
-    def __init__(self, **args):
-        args.setdefault("formatter", JSONFormatter())
-        args.setdefault("exceptions", JSONErrorHandler())
-        args.setdefault("parser", JSONParser())
-        super(JSONApp, self).__init__(**args)
+        return Map(r for r in self.build_rules())
