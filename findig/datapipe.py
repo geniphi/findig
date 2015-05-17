e7468627c0062ef1f61c23ceb2644ea133ccbe36
Te-jé Rodgers <tjd.rodgers@gmail.com>
2015-05-17 11:27:51 -0400

    Updated data pipe documentation

--- findig/datapipe.py
+++ findig/datapipe.py
@@ -4,6 +4,19 @@ from functools import reduce
 
 
 class DataPipe:
+    """
+    An object that folds data over a set of functions.
+
+    :param funcs: A variable list of functions. Each function must take
+        one parameter and return a single value.
+
+    Calling this object with data will pass the data through each one of
+    the functions that is has collected, using the result of one function
+    as the argument for the next function. For example, if the data pipe
+    ``dpipe`` contains the functions ``[f1, f2, ..., fn]``, then 
+    ``dpipe(data)`` is equivalent to ``fn(...(f2(f1(data))))``.
+
+    """
     def __init__(self, *funcs):
         self.funcs = []
 
@@ -16,6 +29,13 @@ class DataPipe:
                 self.funcs.append(func)
 
     def stage(self, func):
+        """
+        Append a function to the data pipes internal list.
+
+        This returns the function that it is called with, so it can be
+        used as a decorator.
+
+        """
         self.funcs.append(func)
         return func
 
